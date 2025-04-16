1f841f9adf27c30dcf3a8e357d86c9be9b2ee167
christopher-r-doucet <cdoucet6302@gmail.com>
2025-04-15 21:11:59 -0700

    Fix duplicate my_matches command causing undefined property error

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -4,12 +4,13 @@ import { leagueCommands } from './league.js';
 import { playerCommands } from './player.js';
 import { helpCommands } from './help.js';
 import { matchCommands } from './match.js';
+import myMatchesCommand from './my-matches.js';
 
 // Create a collection of commands
 const commands = new Collection<string, Command>();
 
 // Add all commands
-const allCommands = [...leagueCommands, ...playerCommands, ...helpCommands, ...matchCommands];
+const allCommands = [...leagueCommands, ...playerCommands, ...helpCommands, ...matchCommands, myMatchesCommand];
 for (const command of allCommands) {
   commands.set(command.data.name, command);
 }
